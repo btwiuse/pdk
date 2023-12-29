64e082b8da5621470e479986455552ab3d9f47e6
navigaid <navigaid@gmail.com>
2023-12-29 15:03:19 +0000

    use sp_std::vec::Vec;

--- substrate/bin/node-template/pallets/poe/src/lib.rs
+++ substrate/bin/node-template/pallets/poe/src/lib.rs
@@ -67,6 +67,7 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use sp_std::vec::Vec;
 
 	// The `Pallet` struct serves as a placeholder to implement traits, methods and dispatchables
 	// (`Call`s) in this pallet.
