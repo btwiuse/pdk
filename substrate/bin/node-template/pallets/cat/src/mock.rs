4689940cc0f9264c0030b7a1751d87f7acdb0938
navigaid <navigaid@gmail.com>
2024-01-26 13:01:16 +0000

    rename: poe -> cat
    git grep -ie poe | cut -d : -f 1 | xargs -L1 sed -i -e s,poe,cat,g -e s,Poe,Cat,g

--- substrate/bin/node-template/pallets/cat/src/mock.rs
+++ substrate/bin/node-template/pallets/cat/src/mock.rs
@@ -1,4 +1,4 @@
-use crate as pallet_poe;
+use crate as pallet_cat;
 use frame_support::traits::{ConstU16, ConstU64};
 use frame_support::parameter_types;
 use sp_core::H256;
@@ -14,7 +14,7 @@ frame_support::construct_runtime!(
 	pub enum Test
 	{
 		System: frame_system,
-		PoeModule: pallet_poe,
+		CatModule: pallet_cat,
 	}
 );
 
@@ -49,7 +49,7 @@ parameter_types! {
 	pub const ProofSizeLimit : u32 = 42;
 }
 
-impl pallet_poe::Config for Test {
+impl pallet_cat::Config for Test {
 	type ProofSizeLimit = ProofSizeLimit;
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
